afdc40cfa046b2ee321e7281744e0457d14b1202
Andrew <andrew.lim@tbwa.com>
2022-04-22 22:53:12 +0800

    before editing index page

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -6,20 +6,25 @@
 
 
 <div
-	class="mx-auto flex max-w-2xl flex-col items-start justify-center border-gray-200 px-4 pb-16 dark:border-gray-700 sm:px-8"
->
+	class="mx-auto flex max-w-2xl flex-col items-start justify-center border-gray-200 px-4 pb-16 dark:border-gray-700 sm:px-8">
+
 	<div class="flex flex-col justify-center items-start max-w-2xl mb-16">
 		<div class="flex flex-col pr-8">
 			<h1 class="mb-3 text-3xl font-bold tracking-tight md:text-5xl py-4">
 				About Me
 			</h1>
 			<h2 class="mb-4">
-				This is my first web application built on Sveltekit.
-				I am a data analyst currently working in an advertising agency. 
+				This is my first web application built on Sveltekit.<br/>
+				I am a data analyst currently working at <a href="https://www.tbwa.com.sg/"><strong>TBWA</strong></a>, an advertising agency.
 			</h2>
-			<h3 class="mb-16">
+			<h3 class="mb-4">
 				
 			</h3>
+		</div>
+
+		<div class="flex flex-col justify-center items-start max-w-2xl mb-16">
+			<div class="flex flex-col pr-8">
+			</div>
 		</div>
 	</div>
 </div>

68bbaef8087a0af576dae9ca20855e934de39f7d
Andrew <andrew.lim@tbwa.com>
2022-04-29 22:19:24 +0800

    update to launch

--- svelte.config.js
+++ svelte.config.js
@@ -9,10 +9,7 @@ import mdsvexConfig from './mdsvex.config.js';
 const config = {
 	extensions: ['.svelte', ...mdsvexConfig.extensions],
 	kit: {
-		adapter: vercel({
-			pages: 'public',
-			assets: 'public'
-		}),
+		adapter: vercel(),
 
 		vite: {
 			// allows vite access to ./posts

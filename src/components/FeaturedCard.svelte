ef96c43399b9f6b82fb140f081c40d34b5c98f04
Andrew <andrew.lim@tbwa.com>
2022-04-30 11:23:40 +0800

    launch

--- src/components/FeaturedCard.svelte
+++ src/components/FeaturedCard.svelte
@@ -8,7 +8,7 @@
 </script>
 
 <a class="w-full transform rounded-xl p-1 hover:scale-[1.01] md:w-1/3" href={`/posts/${Post.slug}`}
-	><div class="flex h-full flex-col justify-between rounded-lg p-2 dark:bg-gray-900">
+	><div class="flex h-full flex-col justify-between rounded-lg p-2">
 		<div class="flex flex-col justify-between md:flex-row">
 			<h4 class="w-full text-md font-medium tracking-tight sm:mb-2 md:text-md">
 				{Post.title}

2a3cb6031c464026b45847bd0a7653dd808a32ee
andreian02 <andreian02.lim@gmail.com>
2022-06-16 19:38:25 +0800

    edit links

--- src/lib/get-works.js
+++ src/lib/get-works.js
@@ -1,29 +1,34 @@
 const images = [
-    { path: 'images/trends.png', id: 'trends', title: 'Travel Intent Dashboard', url: '' },
-    {
-        path: 'images/hdb.png',
-        id: 'hdb',
-        title: 'Singapore Public Housing',
-        url: 'https://public.tableau.com/app/profile/andrei7065/viz/SingaporePublicHousing/YearDashboard'
-    },
-    {
-        path: 'images/botanic_bloom.png',
-        id: 'botanic_bloom',
-        title: 'Botanic Bloom',
-        url: 'https://public.tableau.com/app/profile/andrei7065/viz/BotanicBloominSingapore_/Dashboard1'
-    },
-    {
-        path: 'images/cancellation.png',
-        id: 'cancellation',
-        title: 'Cancellation Dashboard',
-        url: ''
-    },
-    {
-        path: 'images/omega_swatch.png',
-        id: 'omega_swatch',
-        title: 'Omega Swatch Collab',
-        url: 'https://public.tableau.com/app/profile/andrei7065/viz/Planet-Book/Dashboard1'
-    }
+	{
+		path: 'https://user-images.githubusercontent.com/36339564/174061788-fbb6b13c-1444-41b7-96a5-edd7c2eb7ea2.png',
+		id: 'trends',
+		title: 'Travel Intent Dashboard',
+		url: ''
+	},
+	{
+		path: 'https://user-images.githubusercontent.com/36339564/174061495-538c06e9-d943-4bd8-bb9e-38dc6619c58b.png',
+		id: 'hdb',
+		title: 'Singapore Public Housing',
+		url: 'https://public.tableau.com/app/profile/andrei7065/viz/SingaporePublicHousing/YearDashboard'
+	},
+	{
+		path: 'https://user-images.githubusercontent.com/36339564/174061058-f169d426-2f5d-47dd-857c-6615b461bb77.png',
+		id: 'botanic_bloom',
+		title: 'Botanic Bloom',
+		url: 'https://public.tableau.com/app/profile/andrei7065/viz/BotanicBloominSingapore_/Dashboard1'
+	},
+	{
+		path: 'https://user-images.githubusercontent.com/36339564/174061423-4e334f45-fb96-4b4c-8cc3-6646ea3b5120.png',
+		id: 'cancellation',
+		title: 'Cancellation Dashboard',
+		url: ''
+	},
+	{
+		path: 'https://user-images.githubusercontent.com/36339564/174061718-91b2b29a-91a2-4e45-83df-29f9df33559a.png',
+		id: 'omega_swatch',
+		title: 'Omega Swatch Collab',
+		url: 'https://public.tableau.com/app/profile/andrei7065/viz/Planet-Book/Dashboard1'
+	}
 ];
 
 export default images;
